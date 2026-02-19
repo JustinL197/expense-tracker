f2e6bbb0c17e1765812d1db1894f394765c9154f
Justin <justin.sanghak@gmail.com>
2026-02-19 15:06:13 -0500

    Implement user input for adding expenses
    Added functions to prompt user for expense details.

--- index.js
+++ index.js
@@ -1,8 +1,74 @@
 const {addTransaction, listTransactions} = require('./expense');
+const readline = require('readline/promises');
+const {stdin, stdout} = require('process');
 
-console.log("Expense tracker running...");
+const rl = readline.createInterface({
+    input: stdin,
+    output: stdout,
+});
 
-addTransaction('Timmies', 'Food', 100);
+async function getExpenseTitle(){
+    let expenseTitle;
+    while (true){
+        try{
+            expenseTitle = await rl.question("Expense...");
+            if (!expenseTitle){
+                throw new Error("Cannot be empty")
+            }
+            break;
+        } catch(err){
+            console.log(err)
+        }
+    }
+    return expenseTitle
+}
 
-listTransactions();
+async function getExpenseAmount(){
+    let expenseAmount;
+    while (true){
+        try{
+            expenseAmount = await rl.question("Amount...");
+            if (!Number.isFinite(Number(expenseAmount))){
+                throw new Error("Must be a number");
+            }
+            break;
+        } catch(err){
+            console.log(err)
+        }
+    }
+    return Number(expenseAmount)
+}
 
+async function getExpenseCategory(){
+    let expenseCategory;
+    while (true){
+        try{
+            expenseCategory = await rl.question("Category...");
+            if (!expenseCategory){
+                throw new Error("Cannot be empty")
+            }
+            break;
+        } catch(err){
+            console.log(err)
+        }
+    }
+    return expenseCategory
+}
+
+(async function(){
+    if (process.argv[2] === 'add'){
+    try{
+        let title = await getExpenseTitle();
+        let category = await getExpenseCategory();
+        let amount = await getExpenseAmount();
+
+        if (addTransaction(title, category, amount) == true){
+            console.log('Expense added successfully')
+        }
+    }catch(err){
+        console.log(err)
+    }finally{
+        rl.close()
+    }
+}
+})();
