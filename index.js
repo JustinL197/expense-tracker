6b8a95a3442f79c0935781130b18f9098a30598b
Justin <justin.sanghak@gmail.com>
2026-02-25 16:53:55 -0500

    Add deleteTransaction functionality to index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {addTransaction, listTransactions} = require('./expense');
+const {addTransaction, listTransactions, deleteTransaction} = require('./expense');
 const readline = require('readline/promises');
 const {stdin, stdout} = require('process');
 const fs = require('fs');
@@ -56,25 +56,73 @@ async function getExpenseCategory(){
     return expenseCategory
 }
 
+async function getDeleteId(){
+    let id;
+    while (true){
+        try{
+            id = await rl.question("Enter ID to delete...");
+            if (!Number.isFinite(Number(id))){
+                throw new Error("ID must be a number");
+            }
+            break;
+        } catch(err){
+            console.log(err)
+        }
+    }
+    return Number(id)
+}
 
 (async function(){
     if (process.argv[2] === 'add'){
-    try{
-        let title = await getExpenseTitle();
-        let category = await getExpenseCategory();
-        let amount = await getExpenseAmount();
+        try{
+            let title = await getExpenseTitle();
+            let category = await getExpenseCategory();
+            let amount = await getExpenseAmount();
 
-        if (addTransaction(title, category, amount) == true){
-            console.log('Expense added successfully')
+            if (addTransaction(title, category, amount) == true){
+                console.log('Expense added successfully')
+            }
+        }catch(err){
+            console.log(err)
+        }finally{
+            rl.close()
         }
-    }catch(err){
-        console.log(err)
-    }finally{
-        rl.close()
     }
-}
 })();
 
 if (process.argv[2] === 'list'){
-    console.log (listTransactions());
+    const expenseList = listTransactions();
+    let index = 1;
+    for (let transaction of expenseList){
+        console.log(`${index++}...${transaction.title}....${transaction.amount}....${transaction.timestamp}`)
+    }
+    process.exit()
 }
+
+
+(async function(){
+    if (process.argv[2] === 'del'){
+        try{
+            const expenseList = listTransactions();
+            let index = 1;
+            
+            for (let transaction of expenseList){
+                console.log(`${index++}...${transaction.title}...${transaction.amount}...${transaction.timestamp}`)
+            }
+
+            let id = await getDeleteId();
+
+            if (!Number.isFinite(Number(id))){
+                throw new Error("Must be a number")
+            }
+
+            if (!deleteTransaction(id)){
+                throw new Error("Does not exist")
+            }
+        }catch(err){
+            console.log(err)
+        }finally{
+            rl.close()
+        }
+    }
+})();
